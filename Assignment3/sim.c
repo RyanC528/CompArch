8f75831069198554132a2981a3b297589315cd5b
Ryan_Copeland@student.uml.edu <ryanc@DESKTOP-ELG60CG>
2024-03-28 20:05:08 -0400

    base for hw3 done

--- Assignment3/sim.c
+++ Assignment3/sim.c
@@ -32,9 +32,31 @@ void     mem_write_32(uint32_t address, uint32_t value);
 
 */
 
-uint32_t instrct;
-int opcode = 0b0000000;
-int func3 = 0b000;
+uint32_t instr;
+
+uint32_t opcode = 0b0000000;
+uint32_t func3 = 0b000;
+uint32_t func7 = 0b0000000;
+
+uint32_t rs1 = 0b00000;
+uint32_t rs2 = 0b00000;
+uint32_t rsd = 0b00000;
+
+uint32_t i_imm = 0b000000000000;
+
+uint32_t s_imm_1 = 0b0000000;
+uint32_t s_imm_2 = 0b00000;
+
+uint32_t sb_imm_1 = 0b0000000;
+uint32_t sb_imm_2 = 0b00000;
+
+uint32_t u_imm = 0b00000000000000000000;
+
+uint32_t uj_imm = 0b00000000000000000000;
+
+uint32_t rs1_data = 0b00000;
+uint32_t rs2_data = 0b00000;
+uint32_t rsd_data = 0b00000;
 
 char instr_type;
 
@@ -43,9 +65,9 @@ void fetch()
   if ( RUN_BIT == 1){
     //get instruction
 
-    instrct = mem_read_32(CURRENT_STATE.REGS[CURRENT_STATE.PC]);
+    instr = mem_read_32(CURRENT_STATE.REGS[CURRENT_STATE.PC]);
 
-    printf("%ls \n", &instrct);
+    printf("%ls \n", &instr);
 
     //get instruction
 
@@ -63,7 +85,7 @@ void decode()
 {
   //get opcode
 
-  opcode = (instrct >> 6);//get opcode from bits 7-0
+  opcode = instr & 0x7f;//get opcode from bits 7-0. 0x7f = 1111110000000000
 
   //get opcode
 
@@ -71,7 +93,13 @@ void decode()
 
   switch(opcode){
     case 0b0110011: //add and slt
-      instr_type = "r";
+      rsd =(instr >> 7) & 0x1f; //0x1f = 11111 in binary
+      func3 = (instr >> 12) & 0x07; //0x07 = 111 in binary
+      rs1 = (instr >> 15) & 0x1f;
+      rs2 = (instr >> 20) & 0x1f;
+      func7 = (instr>> 25) & 0x7f;
+
+      instr_type = 'r';
       if(func3 == 0b111){//add
 
       }else if(func3 == 0b010){//slt
@@ -80,7 +108,12 @@ void decode()
       break;
 
     case 0b0010011://addi and slli
-      instr_type = "i";
+      i_imm = (instr >> 20) & 0x0fff;// 0x0777 = 111111111111 in binary
+      rs1 = (instr >> 15) & 0x1f;
+      func3 = (instr >> 12) & 0x07;
+      rsd = (instr >> 7) & 0x1f;
+
+      instr_type = 'i';
       if(func3 == 0b000){//addi
 
       }else if(func3 == 0b001){//slli
@@ -89,19 +122,37 @@ void decode()
       break;
 
     case 0b0100011: //sw
-      instr_type = "s";
+      s_imm_1 = (instr >> 25) & 0x7f;
+      rs2 = (instr >> 20) & 0x1f;
+      rs1 = (instr >> 15) & 0x1f;
+      func3 = (instr >> 13) & 0x07;
+      s_imm_2 = (instr >> 7) & 0x1f;
+
+      instr_type = 's';
       break;
 
     case 0b1100011: //bne
-      instr_type = "d";
+      s_imm_1 = (instr >> 25) & 0x7f;
+      rs2 = (instr >> 20) & 0x1f;
+      rs1 = (instr >> 15) & 0x1f;
+      func3 = (instr >> 13) & 0x07;
+      s_imm_2 = (instr >> 7) & 0x1f;
+
+      instr_type = 'b';
       break;
 
     case 0b0010111: //auipc
-      instr_type = "u";
+      u_imm = (instr >> 12) & 0x3fffff;
+      rsd = (instr >> 7) & 0x1f;
+
+      instr_type = 'u';
       break;
 
     case 0b1101111: //jal
-      instr_type = "j";
+      uj_imm = (instr >> 12) & 0x3fffff;
+      rsd = (instr >>7) & 0x1f;
+
+      instr_type = 'j';
       break;
 
   }
@@ -113,32 +164,61 @@ void decode()
 void execute()
 {
   switch(instr_type){
-    case "r": //add and slt
+    case 'r': //add and slt
       if(func3 == 0b111){//add
+        rs1_data = mem_read_32(rs1);
+        rs2_data = mem_read_32(rs2);
+        rsd_data = rs1_data + rs2_data;
+
+        mem_write_32(rsd, rsd_data);
 
       }else if(func3 == 0b010){//slt
+        rs1_data = mem_read_32(rs1);
+        rs2_data = mem_read_32(rs2);
+        rsd_data = rs1_data - rs2_data;
+
+        mem_write_32(rsd, rsd_data);
 
       }
       break;
 
-    case "i": //add and slt
+    case 'i': //addi and slli
       if(func3 == 0b000){//addi
+        rs1_data = mem_read_32(rs1);
+        rsd_data = rs1_data + i_imm;
 
+        mem_write_32(rsd, rsd_data);
       }else if(func3 == 0b001){//slli
+        rs1_data = mem_read_32(rs1);
+        rsd_data = i_imm - rs1_data;
+
+        mem_write_32(rsd, rsd_data);
 
       }
       break;
 
-    case "s": //add and slt
+    case 's': //sw
+      rs1_data = mem_read_32(rs1);
+      mem_write_32(rsd,rs1_data);
       break;
 
-    case "b": //add and slt
+    case 'b': //bne
+      rs1_data = mem_read_32(rs1);
+      rs2_data = mem_read_32(rs1);
+      
+      if((rs1_data - rs2_data) != 0){
+        NEXT_STATE.PC = (sb_imm_1 >> 5) | sb_imm_2;
+      }
       break;
 
-    case "u": //add and slt
+    case 'u': //auipc
+      uint32_t pc_update = 0b000000000000;
+      pc_update = pc_update | (u_imm >>11);
       break;
 
-    case "b": //add and slt
+    case 'j': //jal
+      uint32_t pc_update = 0b000000000000;
+      pc_update = pc_update | (u_imm >>11);
       break;
   }
 
