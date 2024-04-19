70aa6d80f4dbf1baccb76553eb56cf1378f02d6a
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2024-04-19 19:56:55 -0400

    Update main.c

--- HW5/main.c
+++ HW5/main.c
@@ -1,5 +1,22 @@
 #include "func.c"
 
 int main(int argc, char *argv[]){
+
+int A[N][N], B[N][N], C[N][N];
+int i, j, k;
+
+// naive 
+for (i=0; i<N; i++)
+  for (j=0; j<N; j++)
+    for (k=0; k<N; k++)
+      C[i][j] += A[i][k]+B[k][j];
+
+// smart
+for (i=0; i<N; i++)
+  for (k=0; k<N; k++)
+    for (j=0; j<N; j++)
+      C[i][j] += A[i][k]+B[k][j];
+
+  return 0;
   
 }
