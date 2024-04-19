8a3901d7d5509b9011363730ba0d823c522978bf
RyanC528 <123291263+RyanC528@users.noreply.github.com>
2024-04-19 18:44:01 -0400

    Create samplecode.c

---
+++ HW5/samplecode.c
@@ -0,0 +1,15 @@
+#define N 4
+int A[N][N], B[N][N], C[N][N];
+int i, j, k;
+
+// naive 
+for (i=0; i<N; i++)
+  for (j=0; j<N; j++)
+    for (k=0; k<N; k++)
+      C[i][j] += A[i][k]+B[k][j];
+
+// smart
+for (i=0; i<N; i++)
+  for (k=0; k<N; k++)
+    for (j=0; j<N; j++)
+      C[i][j] += A[i][k]+B[k][j];
